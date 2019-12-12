12c07993a4ede8cc63e3e60287dd709d8a85cda7
kennethlaqui <kennethlaqui@yahoo.com>
2019-12-13 02:12:31 +0800

    Change return value for user

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    return $request->user()->only(['user_id_','user_nme']);
 });
 
 /**
