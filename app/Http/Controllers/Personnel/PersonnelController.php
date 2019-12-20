0786a618b0bc025935af9f5c8584cccc8f877dc3
kennethlaqui <kennethlaqui@yahoo.com>
2019-12-21 00:58:14 +0800

    Modify Directories

--- app/Http/Controllers/Personnel/PersonnelController.php
+++ app/Http/Controllers/Personnel/PersonnelController.php
@@ -11,10 +11,12 @@ class PersonnelController extends Controller
     public function directories(Request $request)
     {
         $directories = DB::table('q_payr_dir')
-            ->select('cntrl_no', 'group_no', 'year____', 'month___',
-                    'part____', 'seqn_num', 'strt_dte', 'last_dte',
-                    'remarks_', 'w2_year_', 'status__', 'appl_prd')
-            ->where('primekey', $request->primekey)
+            ->where('q_payr_dir.primekey', $request->primekey)
+            ->where('q_paygroup.primekey', $request->primekey)
+            ->join('q_paygroup', 'q_paygroup.group_no', '=', 'q_payr_dir.group_no')
+            ->select('q_payr_dir.cntrl_no', 'q_paygroup.descript', 'q_payr_dir.year____', 'q_payr_dir.month___',
+                    'q_payr_dir.part____', 'q_payr_dir.seqn_num', 'q_payr_dir.strt_dte', 'q_payr_dir.last_dte',
+                    'q_payr_dir.remarks_', 'q_payr_dir.w2_year_', 'q_payr_dir.status__', 'q_payr_dir.appl_prd')
             ->get();
         
         return response()->json($directories, 200);
